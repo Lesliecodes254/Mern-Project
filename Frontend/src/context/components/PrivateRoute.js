fab2f11ef429785038962b4db624c426aba0333e
Lesliecodes254 <kimberlyombima254@gmail.com>
2025-11-26 10:18:44 +0300

    Create PrivateRoute.js

---
+++ Frontend/src/context/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+// src/components/PrivateRoute.js
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const PrivateRoute = ({ children }) => {
+  const { token, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-purple-600"></div>
+      </div>
+    );
+  }
+
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
